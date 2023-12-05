2635cc0849967592cad60bdb2b89bd516042eb51
Tanner <thtrout24@yahoo.com>
2023-12-04 22:05:36 -0800

    Added JS files

---
+++ 5-JavaScript/1-variables.js
@@ -0,0 +1,33 @@
+console.log("Hello world")
+
+// Statements: JavaScript code is composed of statements that perform specific actions. Each statement typically ends with a semicolon (;), although it is not always required.
+
+// Comments: You can add comments in JavaScript to provide explanations or make notes within the code. There are two types of comments: single-line comments, starting with //, and multi-line comments, enclosed between /* and */.
+
+// Case sensitivity: JavaScript is case-sensitive, meaning that uppercase and lowercase letters are treated differently. For example, "variable" and "Variable" would be considered as two separate variables.
+
+// Whitespace: JavaScript ignores spaces, tabs, and line breaks (collectively called whitespace) that are not necessary for code execution. However, whitespace can enhance code readability.
+
+// Blocks: Code blocks in JavaScript are enclosed in curly braces ({}) and are used to group statements together. Blocks are often used in control structures like loops and conditional statements.
+
+//Variables
+
+//firstName
+//lastName
+let firstName = "John"
+let lastName = "Smith"
+console.log("This is the name of the user:", firstName, lastName);
+
+var myFavoriteGame = "Animal Crossing";
+console.log("My favorite game is", myFavoriteGame)
+
+if (true) {
+    let myFavoriteDrink = "coffee";
+    var mySecondFavoriteDrink = "tea";
+};
+
+console.log(mySecondFavoriteDrink);
+
+//Constant
+const gravity = "9.8"
+const birthday = "June 23"
---
+++ 5-JavaScript/app.js
@@ -0,0 +1,18 @@
+//String
+let firstName = "John";
+console.log("The type of name:", typeof firstName);
+
+//Number
+let age = 27;
+console.log("The type of age:", typeof age);
+
+//Boolean
+let isStudent = false;
+let isTeacher = true;
+console.log("The type of isStudent:", typeof isStudent);
+console.log("The type of isTeacher:", typeof isTeacher);
+
+//Null - Intentional absence of data
+let girlfriend = null;
+console.log("The type of girlfriend:", typeof girlfriend);
+
